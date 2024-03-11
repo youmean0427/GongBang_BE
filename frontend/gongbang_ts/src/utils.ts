fdcdb7b4771ede77f879595659d4716b6754ae3d
youmean0427 <youmean0427@gmail.com>
2024-03-12 00:59:14 +0900

    [git] Git Separation

--- frontend/gongbang_ts/src/utils.ts
+++
@@ -1,6 +0,0 @@
-export const getCookie = (name: string) => {
-  const cookieValue = document.cookie.match(
-    "(^|;)\\s*" + name + "\\s*=\\s*([^;]+)"
-  );
-  return cookieValue ? cookieValue.pop() : null;
-};

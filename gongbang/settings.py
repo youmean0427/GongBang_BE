5c514d0273861e718d6814cc2511eac60379e0ff
youmean0427 <youmean0427@gmail.com>
2024-04-23 23:06:30 +0900

    [test] Deploy

--- gongbang/settings.py
+++ gongbang/settings.py
@@ -15,10 +15,10 @@ import os
 from datetime import timedelta
 import django_heroku
 import dj_database_url 
-
+import pymysql
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+pymysql.install_as_MySQLdb()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'GongBang',
         'USER': 'root',
-        'PASSWORD' : os.environ.get('DATABASE_PASSWORD'),
+        'PASSWORD' : os.environ['DATABASE_PASSWORD'],
         'HOST': 'svc.sel5.cloudtype.app',
         'PORT': '32240'
     }
